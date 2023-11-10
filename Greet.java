765c1f1f4f104393a4a5367388206b899dcad5b9
Rayster Ramos <rjrfrst@gmail.com>
2023-11-10 12:17:07 +0000

    Testing part 2

--- Greet.java
+++ Greet.java
@@ -3,6 +3,6 @@ public class Greet {
     public static void main(String[] args) {
         String salutation = “Good morning”;  // MODIFIED
         System.out.println(salutation);  // MODIFIED
-        System.out.println(“How are you?“);
+        System.out.println(“How are you?“); //Test part 2 
     }
 }
